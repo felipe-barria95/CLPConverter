002e221f0dffbfdd6ec715ea7eee30fba43fcfa0
felipe-barria95 <frbarria@uc.cl>
2022-11-13 21:05:06 -0300

    add method

--- clpconverter/converter/app_admin/clp_per_uf.py
+++ clpconverter/converter/app_admin/clp_per_uf.py
@@ -4,3 +4,14 @@ from django.contrib import admin
 @admin.register(CLPPerUFModel)
 class CLPPerUFAdmin(admin.ModelAdmin):
     list_display = ['date', 'value']
+
+    actions=["fix_value"]
+
+    def fix_value(self, request, query):
+        values = CLPPerUFModel.objects.all()
+        for v in values:
+            if 1000000 < v.value < 10000000:
+                v.value = v.value/100
+            elif 100000 < v.value< 1000000:
+                v.value= v.value /10
+            v.save()
